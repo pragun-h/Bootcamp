4faca8c1ac8b8ac04b76a15f943708f4d28c9680
Pragun H <126651351+pragun-h@users.noreply.github.com>
2026-06-17 09:18:25 +0530

    Add files via upload

---
+++ HackerEarth/binarySearch.py
@@ -0,0 +1,20 @@
+def binarySearch(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+arr = [2, 4, 6, 8, 10, 12]
+print(binarySearch(arr, 8))  
+print(binarySearch(arr, 5))   
---
+++ HackerEarth/bubbleSort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+arr = [5, 2, 8, 1, 3]
+print(bubbleSort(arr))
