9fb6caad502dea537d291c3af4968d9704ef9c71
Pragun H <126651351+pragun-h@users.noreply.github.com>
2026-06-17 09:15:57 +0530

    Update chefAndDolls.py

--- CODECHEF/chefAndDolls.py
+++ CODECHEF/chefAndDolls.py
@@ -1,1 +1,12 @@
+t = int(input())
 
+for _ in range(t):
+    n = int(input())
+    dolls = list(map(int, input().split()))
+
+    ans = 0
+
+    for doll in dolls:
+        ans ^= doll
+
+    print(ans)
