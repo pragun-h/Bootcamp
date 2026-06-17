8a9f4e9bc8370840a393ba10552839a6b674fbea
Pragun H <126651351+pragun-h@users.noreply.github.com>
2026-06-17 09:24:42 +0530

    Delete CodeChef directory

--- CodeChef/ATM.py
+++
@@ -1,9 +0,0 @@
-withdraw, balance = input().split()
-
-withdraw = int(withdraw)
-balance = float(balance)
-
-if withdraw % 5 == 0 and withdraw + 0.50 <= balance:
-    balance -= (withdraw + 0.50)
-
-print(f"{balance:.2f}")
